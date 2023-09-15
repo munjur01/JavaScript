8c1e953887039dec849f7f45afbedd05f69c1b48
Munjur Alom <97244984+MunjurAlom@users.noreply.github.com>
2023-09-15 12:35:30 +0600

    Add files via upload

---
+++ 2. Variables & Constants/js_constants_&_keywords.js
@@ -0,0 +1,11 @@
+var a = 30;
+a = a + 5;
+document.write("This is Variable: ",a);
+// Constant Declaration
+
+const b = 10;
+document.write("This is Constant: ",b);
+// b = b + 3; is not possible
+
+/* Keywords
+const, var, prompt..*/
---
+++ 2. Variables & Constants/js_variables_part_1&2.js
@@ -0,0 +1,10 @@
+// Variables Declaration 
+var myName;
+let x,y, sum;
+myName = "Munjur Alom";
+var yName = 45;
+x = 56;
+y = 40;
+document.write("My Name is: ",myName);
+sum = x + y;
+document.write("Summation of X & Y is: ",sum);
